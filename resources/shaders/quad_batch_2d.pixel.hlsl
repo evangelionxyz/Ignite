8885fc555cbf2fafa522b2630abd5c7176a45d49
Evangelion Manuhutu <evangelion11.m@gmail.com>
2025-05-12 01:46:50 +0900

    Added texture alpha blending

--- resources/shaders/quad_batch_2d.pixel.hlsl
+++ resources/shaders/quad_batch_2d.pixel.hlsl
@@ -15,5 +15,9 @@ SamplerState samplerState : register(s0);
 float4 main(PSInput input) : SV_TARGET
 {
     float4 texColor = textures[input.texIndex].Sample(samplerState, input.texCoord * input.tilingFactor);
+    
+     // Discard pixel if alpha is zero
+    clip(texColor.a == 0.0f ? -1.0f : 1.0f);
+
     return input.color * texColor;
 }
