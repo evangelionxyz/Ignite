16a29341524f4d5275bca755c4af8026f083d54e
Evangelion Manuhutu <evangelion11.m@gmail.com>
2025-06-09 02:26:17 +0900

    Fixed imgui SRGB to Linear texture

--- resources/shaders/imgui.pixel.hlsl
+++ resources/shaders/imgui.pixel.hlsl
@@ -13,6 +13,7 @@ sampler sampler0 : register(s0);
 float4 main(PS_INPUT input) : SV_Target
 {
     float4 texColor = texture0.Sample(sampler0, input.uv);
+    texColor = float4(SRGBToLinear(texColor.rgb), texColor.a);
     float4 color = float4(SRGBToLinear(input.col.rgb), input.col.a);
     return color * texColor;
 }
